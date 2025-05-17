a23c7f94543b283430b5e190fadcb7085d0a463b
Evgen992 <151379688+Evgen992@users.noreply.github.com>
2025-05-17 20:55:40 +0300

    продолжаем работу

--- main.rb
+++ main.rb
@@ -3,7 +3,8 @@ require_relative "film"
 require_relative "book"
 
 product = Product.new
-film = Film.new 
+film = Film.new(1, 290)
 book = Book.new
+
 
 puts "фильм Леон стоит #{film.price} рублей"
--- product.rb
+++ product.rb
@@ -1,6 +1,7 @@
 class Product
   attr_accessor :quantity, :price
   def initialize(quantity = nil, price = nil)
+  
   @quantity = quantity
   @price = price 
   end
